30d09a178580f46427f9cc32af250683e1066475
seunghyecho <tmdgp523@naver.com>
2021-02-01 15:53:43 +0900

    overlay html,css,js 추가

--- js/combined.js
+++ js/combined.js
@@ -1,8 +1,36 @@
 $(document).ready(function(){
-    console.log('header');
+    console.log('footer');
 })
 $(document).ready(function(){
-    console.log('footer');
+    console.log('header');
+
+    var $headerIconSearch = $('.header_icon .searchBtn');
+    var $headerIconMenu = $('.header_icon .gnbBtn');
+    var $overlay = $('.overlay');
+    var $overlaySearch = $('.overlay.search');
+    var $overlayGnb = $('.overlay.gnb');
+    var $closeBtn = $('.closeBtn');
+    console.log($closeBtn)
+
+    $($overlay).hide();
+
+    $headerIconSearch.on('click', function(e){
+        e.preventDefault();
+        $($overlaySearch).fadeIn();
+        $('html,body').css({'overflow': 'hidden'});
+    });
+
+    $headerIconMenu.on('click', function(e){
+        e.preventDefault();
+        $($overlayGnb).fadeIn();
+        $('html,body').css({'overflow': 'hidden'});
+    });
+    $closeBtn.on('click', function(){
+        $($overlay).fadeOut();
+        $('html,body').css({'overflow': 'visible'});
+    });
+
+
 })
 $(document).ready(function () {
     console.log('main intro');
