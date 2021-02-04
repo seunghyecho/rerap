48e0b7c1b597557ab6c1f43a91e45d1f6eb07bec
seunghyecho <tmdgp523@naver.com>
2021-02-04 19:17:13 +0900

    헤더 링크클릭시 스크롤링

--- js/combined.js
+++ js/combined.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
     console.log('footer');
 })
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('header');
+
+    // Smooth Anchor Scroll
+    var $gnb = $('#gnb');
+    $gnb.on('click', function (e) {
+
+        // var targetId = e.target.getAttribute("href");
+        // window.scrollTo({
+        //     top: targetId === "#" ? 0 : document.querySelector(targetId).offsetTop,
+        //     behavior: "smooth"
+        // });
+
+        var _this = e.target;
+        var _focusId = _this.getAttribute("href");
+        var _heigth = $(_focusId).offset();
+
+        $('body').animate({ scrollTop: _heigth.top }, 1000);
+
+    });
 
     var $headerIconSearch = $('.header_icon .searchBtn');
     var $headerIconMenu = $('.header_icon .gnbBtn');
@@ -13,20 +31,20 @@ $(document).ready(function(){
 
     $($overlay).hide();
 
-    $headerIconSearch.on('click', function(e){
+    $headerIconSearch.on('click', function (e) {
         e.preventDefault();
         $($overlaySearch).fadeIn();
-        $('html,body').css({'overflow': 'hidden'});
+        $('html,body').css({ 'overflow': 'hidden' });
     });
 
-    $headerIconMenu.on('click', function(e){
+    $headerIconMenu.on('click', function (e) {
         e.preventDefault();
         $($overlayGnb).fadeIn();
-        $('html,body').css({'overflow': 'hidden'});
+        $('html,body').css({ 'overflow': 'hidden' });
     });
-    $closeBtn.on('click', function(){
+    $closeBtn.on('click', function () {
         $($overlay).fadeOut();
-        $('html,body').css({'overflow': 'visible'});
+        $('html,body').css({ 'overflow': 'visible' });
     });
 
 
